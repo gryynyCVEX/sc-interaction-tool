bdb9079635f8b1a59bce63c29eb035f94ff4d911
Andrii Hrynchuk <andrii.hrynchuk@cvexlabs.com>
2025-01-14 10:21:12 +0000

    Update MasterPage.tsx

--- src/components/MasterPage.tsx
+++ src/components/MasterPage.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { useAppKitProvider } from '@reown/appkit/react';
 import { ethers, BrowserProvider } from "ethers";
 import ABIUploader from "./ABIUploader";
